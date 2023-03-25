0759dfc58f1706d3c5254c74c3f3a0599e8759a5
Barnoch Anas <anasbarnoch@gmail.com>
2023-03-25 13:55:09 +0000

    LM-7 Get all books

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -24,10 +24,10 @@ class BookController extends Controller
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
-    {
-        //
-    }
+    // public function create()
+    // {
+    //     //
+    // }
 
     /**
      * Store a newly created resource in storage.
@@ -48,10 +48,10 @@ class BookController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Book $book)
-    {
-        //
-    }
+    // public function edit(Book $book)
+    // {
+    //     //
+    // }
 
     /**
      * Update the specified resource in storage.
